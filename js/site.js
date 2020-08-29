eab6e3d8b1074bd7ef15049a7cc42b4d7b3c7e4b
Fumeng Yang <yang.form@gmail.com>
2020-08-28 20:47:27 -0400

    compiled

--- js/site.js
+++
@@ -1,6 +0,0 @@
-
-// @depends ../node_modules/jquery/dist/jquery.js
-// @depends ../node_modules/foundation-sites/dist/js/foundation.js
-// @depends ../node_modules/foundation-sites/js/foundation.abide.js
-//
-
